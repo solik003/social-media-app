ed84156dd244ae78ce2e6ebfe9382e27420efd76
solik003 <malugasola@gmail.com>
2024-09-13 12:13:51 +0300

    add login functionality with Context API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,17 @@ import Profile from "./pages/profile/Profile";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
 
 function App() {
+  const { user } = useContext(AuthContext);
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/" element={user ? <Home /> : <Register />} />
+        <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
+        <Route path="/register" element={user ? <Navigate to="/" /> : <Register />} />
         <Route path="/profile/:id" element={<Profile />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
