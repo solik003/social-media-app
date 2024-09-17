d661783e52a77281c78e107e67b8e3305068c21f
solik003 <malugasola@gmail.com>
2024-09-17 12:31:21 +0300

    call current user with Context API

--- frontend/src/components/topbar/Topbar.jsx
+++ frontend/src/components/topbar/Topbar.jsx
@@ -2,8 +2,12 @@ import "./Topbar.css";
 import { Search,Person,Chat,Notifications } from "@mui/icons-material";
 import './Topbar.css';
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
 
 const Topbar = () => {
+  const { user } = useContext(AuthContext);
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
   return (
     <div className="topbarContainer">
       <div className="topbarLeft">
@@ -36,7 +40,10 @@ const Topbar = () => {
             <span className="topbarIconBadge">1</span>
           </div>
         </div>
-        <img src="/assets/person/1.jpeg" alt="" className="topbarImg" />
+        <Link to={`/profile/${user.username}`}>
+          <img src={user.profilePicture ? PF+user.profilePicture: PF+'person/noAvatar.png'} alt="" className="topbarImg" />
+        </Link>
+        
       </div>
     </div>
   )
